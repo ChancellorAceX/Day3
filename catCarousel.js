a52390ea66b5b4decbfad1bed72de0027d93ab77
Adam Brown <adam.edward.brown@gmail.com>
2020-04-22 15:04:22 -0400

    completed catcarousel and fizzbuzz

--- catCarousel.js
+++ catCarousel.js
@@ -1,8 +1,7 @@
-/*$('.thumbnail').on('click', e => {
-  console.log(e.target);
-});
-*/
-
 $('.thumbnail').click(function(e){
-    $('.hero img').replaceWith($(e.target).clone())})
+  const eSrc = $(e.target).is('a')?$(e.target).find('img').attr('src'):e.target.src;
+  const eAlt = $(e.target).is('a')?$(e.target).find('img').attr('alt'):e.target.alt;
+  $('.hero img').attr('src',eSrc);
+  $('.hero img').attr('alt',eAlt);
+});
 
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,15 @@
-// your code here
+function count(num){
+  for(let i=1;i<=num;i++){
+    currentString = i%3===0&&i%5===0?' fizzbuzz':i%3===0?' fizz':i%5===0?' buzz':'' 
+    currentValue = i%3===0&&i%5===0?'fizzbuzz':i%3===0?'fizz':i%5===0?'buzz':i
+    $('.js-results').append($("<div class='fizz-buzz-item"+currentString+"'></div>"))
+    $('.js-results div').last().append($("<span>"+currentValue+"</span>"))
+    console.log(currentValue)
+  }
+}
+
+$('#number-chooser').click(function(event){
+  event.preventDefault();
+  $('.js-results').empty()
+  $('.js-results').text(count($('#number-choice').val()))
+})
